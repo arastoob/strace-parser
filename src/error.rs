62acf00e2ecec061345ad5da8a9190eb0f263492
Arastoo Bozorgi <a.bozorgi67@gmail.com>
2022-04-04 17:26:23 -0230

    Siplify the error module and make it private

--- src/error.rs
+++ src/error.rs
@@ -7,27 +7,14 @@ use std::fmt;
 pub enum Error {
     /// Something not found
     NotFound(String),
-
-    IO(std::io::Error),
-
-    Unknown(String),
 }
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            &Error::IO(ref e) => Some(e),
-            _ => None,
-        }
-    }
-}
+impl std::error::Error for Error { }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             &Error::NotFound(ref detail) => write!(f, "{} not found", detail),
-            &Error::IO(ref err) => write!(f, "IO error: {}", err),
-            &Error::Unknown(ref detail) => write!(f, "Unknown error: {}", detail),
         }
     }
 }
