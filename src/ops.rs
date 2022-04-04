025aadc32fca4d3c88718155a426d32e173982ee
Arastoo Bozorgi <a.bozorgi67@gmail.com>
2022-04-04 16:52:19 -0230

    Define setter methods and implement Display for Operation

--- src/ops.rs
+++ src/ops.rs
@@ -1,28 +1,76 @@
+use std::fmt;
+use std::fmt::Formatter;
 
 #[derive(Debug, PartialEq)]
 pub enum OperationType {
     Read,
-    ReadAt,
-    Write,
-    Mkdir,
+    Write(String), // the input string is the content of the write op
+    Mkdir(String), // the input string is the mode, e.g, 0777
     Mknod,
     OpenAt,
-    Other
+    GetRandom,
+    NoOp
 }
 pub struct Operation {
     pub kind: OperationType,
     pub size: Option<usize>,
-    pub path: String,
+    pub offset: Option<i32>,
+    pub path: Option<String>,
 }
 
 impl Operation {
-    pub fn new(kind: OperationType, size: Option<usize>, path: String) -> Self {
+    fn new(kind: OperationType, size: Option<usize>, offset: Option<i32>, path: Option<String>) -> Self {
         Operation {
             kind,
             size,
+            offset,
             path,
         }
     }
 
+    pub fn read(size: usize, offset: i32, path: String) -> Self {
+        Operation::new(OperationType::Read, Some(size), Some(offset), Some(path))
+    }
 
+    pub fn no_op() -> Self {
+        Operation::new(OperationType::NoOp, None, None, None)
+    }
+
+    pub fn mkdir(mode: String, path: String) -> Self {
+        Operation::new(OperationType::Mkdir(mode), None, None, Some(path))
+    }
+
+    pub fn mknod(size: usize, offset: i32, path: String) -> Self {
+        Operation::new(OperationType::Mknod, Some(size), Some(offset), Some(path))
+    }
+
+    pub fn open_at(offset: i32, path: String) -> Self {
+        Operation::new(OperationType::OpenAt, None, Some(offset), Some(path))
+    }
+
+    pub fn write(content: String, size: usize, offset: i32, path: String) -> Self {
+        Operation::new(OperationType::Write(content), Some(size), Some(offset), Some(path))
+    }
+
+    pub fn get_random(size: usize) -> Self {
+        Operation::new(OperationType::GetRandom, Some(size), None, None)
+    }
+}
+
+impl fmt::Display for Operation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let path = self.path.clone().unwrap_or("".to_string());
+        let offset = self.offset.unwrap_or(0);
+        let size = self.size.unwrap_or(0);
+
+        match &self.kind {
+            &OperationType::Mkdir(ref mode) => write!(f, "mkdir({}, {})", path, mode),
+            &OperationType::Mknod => write!(f, "mknod({}, {}, {})", path, offset, size),
+            &OperationType::Read => write!(f, "read({}, {}, {})", path, offset, size),
+            &OperationType::Write(ref content) => write!(f, "write({}, {}, {}, {})", path, content, offset, size),
+            &OperationType::OpenAt => write!(f, "open({}, {})", path, offset),
+            &OperationType::GetRandom => write!(f, "get_random({})", size),
+            &OperationType::NoOp => write!(f, "no-op"),
+        }
+    }
 }
