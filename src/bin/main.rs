1f9718c6170a1c8ee6faf1203eaaac7ef0651dc9
Arastoo Bozorgi <a.bozorgi67@gmail.com>
2022-08-04 13:58:23 -0230

    Use the ordered dependency graph for printing the processes

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -21,17 +21,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     let mut parser = Parser::new(args.path);
-    let processes = parser.parse()?;
+    let mut dep_graph = parser.parse()?;
 
     let out_file = OpenOptions::new()
         .create(true)
         .write(true)
         .append(false)
         .open(args.out)?;
-
     let mut writer = BufWriter::new(out_file);
-    for process in processes {
-        writer.write(process.to_string().as_ref())?;
+
+    let mut available_set = dep_graph.available_set()?;
+    while !available_set.is_empty() {
+        for process in available_set.iter() {
+            writer.write(process.to_string().as_ref())?;
+        }
+
+        available_set = dep_graph.available_set()?;
     }
     writer.flush()?;
 
