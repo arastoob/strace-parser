f20b72ad10c23767ec589e00056b7ef85585a45a
Arastoo Bozorgi <a.bozorgi67@gmail.com>
2022-08-05 10:06:44 -0230

    Do not expose the DependencyGraph struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ mod parser;
 mod process;
 
 // re-export the required modules
-pub use deps::DependencyGraph;
 pub use op::{Operation, OperationType};
 pub use parser::{FileType, Parser};
 pub use process::Process;
